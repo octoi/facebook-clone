d78bf0d0102271f45c3f3ebfeeb6022d59ae8505
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-29 13:33:55 +0530

    completed the tab bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,25 +8,46 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: appWhite,
-      appBar: AppBar(
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
         backgroundColor: appWhite,
-        elevation: 0.0,
-        title: Text(
-          "facebook",
-          style: TextStyle(
-            color: appBlue,
-            fontSize: 28.0,
-            fontWeight: FontWeight.w700,
+        appBar: AppBar(
+          backgroundColor: appWhite,
+          elevation: 0.0,
+          title: Text(
+            "facebook",
+            style: TextStyle(
+              color: appBlue,
+              fontSize: 28.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          actions: [
+            CustomGrayButton(icon: FontAwesomeIcons.search),
+            SizedBox(width: 10.0),
+            CustomGrayButton(icon: FontAwesomeIcons.facebookMessenger),
+            SizedBox(width: 10.0),
+          ],
+          bottom: TabBar(
+            indicatorColor: appBlue,
+            indicatorSize: TabBarIndicatorSize.label,
+            unselectedLabelColor: appGrayDark,
+            labelColor: appBlue,
+            tabs: [
+              Tab(icon: Text('Home', style: TextStyle(fontSize: 16.0))),
+              Tab(icon: Text('Favorites', style: TextStyle(fontSize: 16.0))),
+              Tab(icon: Text('Recent', style: TextStyle(fontSize: 16.0))),
+              Container(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.tune),
+                  color: appGrayDark,
+                ),
+              ),
+            ],
           ),
         ),
-        actions: [
-          CustomGrayButton(icon: FontAwesomeIcons.search),
-          SizedBox(width: 10.0),
-          CustomGrayButton(icon: FontAwesomeIcons.facebookMessenger),
-          SizedBox(width: 10.0),
-        ],
       ),
     );
   }
