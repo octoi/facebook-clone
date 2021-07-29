2a61858a08b606052c32ab1409d4d749cd7a72f4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-29 12:49:23 +0530

    title

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:facebook/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appWhite,
+      appBar: AppBar(
+        backgroundColor: appWhite,
+        elevation: 0.0,
+        title: Text(
+          "facebook",
+          style: TextStyle(
+            color: appBlue,
+            fontSize: 28.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
   }
 }
