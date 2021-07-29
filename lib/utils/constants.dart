82bb0b362d7728fda544526ea279997c4e2fa863
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-29 12:49:21 +0530

    colors

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appBlue = Color(0xFF1877F2);
+const Color appWhite = Color(0xFFFFFFFF);
+const Color appBlack = Color(0xFF050505);
+const Color appGray = Color(0xFFE4E6EB);
