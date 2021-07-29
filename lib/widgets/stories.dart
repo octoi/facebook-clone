a670bc3a668d6654127ae4bfac5e7c33da8ce354
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-29 15:49:13 +0530

    my story

--- lib/widgets/stories.dart
+++ lib/widgets/stories.dart
@@ -1,3 +1,4 @@
+import 'package:facebook/utils/constants.dart';
 import 'package:facebook/utils/data.dart';
 import 'package:flutter/material.dart';
 
@@ -13,20 +14,38 @@ class Stories extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Container(
-            height: 180.0,
-            width: 120.0,
-            margin: EdgeInsets.only(right: 10.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              image: DecorationImage(
-                image: NetworkImage(currentUser.profile),
-                fit: BoxFit.cover,
+          Stack(
+            children: [
+              Container(
+                height: 180.0,
+                width: 120.0,
+                margin: EdgeInsets.only(right: 10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  image: DecorationImage(
+                    image: NetworkImage(currentUser.profile),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-            child: Stack(
-              children: [],
-            ),
+              Positioned(
+                bottom: 10,
+                left: 10,
+                child: CircleAvatar(
+                  radius: 13.0,
+                  backgroundColor: appWhite,
+                  child: CircleAvatar(
+                    backgroundColor: appBlue,
+                    radius: 12.0,
+                    child: Icon(
+                      Icons.add,
+                      size: 20.0,
+                      color: appWhite,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
